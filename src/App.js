9e9b1a466741d757f67a59880c2b6e1eb0073204
Barri <info@bdesignstudio.co.uk>
2020-07-01 17:48:45 +0100

    make new app.js

--- src/App.js
+++ src/App.js
@@ -8,13 +8,27 @@ class App extends Component {
   render(){
     const name = 'Zaghart';
     const foo =() => 'Bar';
-
-
+    const loading = false;
+      // if(loading) {
+      //   return <h3>loading..</h3>
+      // }
+    const showName = true;
     return (
       <div className="App">
-        <h1>Hello {name.toUpperCase()}</h1>
+        {/* <h1>Hello {name.toUpperCase()}</h1>
+
         <h1>Hello {foo()}</h1>
-        <h1>and:  {this.scopeVar()}</h1>
+        {/*use of ternary operator in jsx*/}
+      {/* {loading ? <h3>loading..</h3>: <h1> and:  {this.scopeVar()}</h1> }
+        <h1>and:  {this.scopeVar()}</h1> */} 
+
+        { loading ? (
+          <h4>loading..</h4>
+        ) : (
+        // <h1>Hello { showName ? name : null }</h1> or use syntax below && means show the name only if showName = true
+        <h1>Hello { showName && name }</h1>
+        )}
+
       </div>
     
     );
