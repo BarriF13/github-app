c3a100b228bb521c2153e1f22e2835d25117ec7b
Barri <info@bdesignstudio.co.uk>
2020-07-01 17:01:02 +0100

    Use Javascript

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React,{Fragment, Component} from 'react';
+import React,{Component} from 'react';
 
 import './App.css';
 
 class App extends Component {
   render(){
-    return (
+    // return (
       // <div className="App">
       //   <h1>Hello Barri</h1>
       //   <h1>Hello my parent is a div</h1>
       // </div>
-      <Fragment>
-        <h1>I am an h1 in a fragment</h1>
-      </Fragment>
-    );
+      // <Fragment>
+      //   <h1>I am an h1 in a fragment</h1>
+      // </Fragment>
+    // );
+
+    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hello from react with js'));
   }
 }
 
