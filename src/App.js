1574234f03d7906c439dc0582a02243b1a88b88c
Barri <info@bdesignstudio.co.uk>
2020-07-01 17:14:25 +0100

    USe of conditional and ternary operator

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import React,{Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render(){
-    // return (
-      // <div className="App">
-      //   <h1>Hello Barri</h1>
-      //   <h1>Hello my parent is a div</h1>
-      // </div>
-      // <Fragment>
-      //   <h1>I am an h1 in a fragment</h1>
-      // </Fragment>
-    // );
 
-    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hello from react with js'));
+  scopeVar = ()=> 'I am out of scope'
+  render(){
+    const name = 'Zaghart';
+    const foo =() => 'Bar';
+
+
+    return (
+      <div className="App">
+        <h1>Hello {name.toUpperCase()}</h1>
+        <h1>Hello {foo()}</h1>
+        <h1>and:  {this.scopeVar()}</h1>
+      </div>
+    
+    );
+    
   }
 }
 
