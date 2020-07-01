5454914c72b5df7dcdae53857b2081c9704ec552
Barri <info@bdesignstudio.co.uk>
2020-07-01 16:58:16 +0100

    Use of Fragment or div on jsx

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React,{Fragment, Component} from 'react';
 
 import './App.css';
 
-class App extends React.Component {
+class App extends Component {
   render(){
     return (
-      <div className="App">
-        <h1>Hello Barri</h1>
-      </div>
+      // <div className="App">
+      //   <h1>Hello Barri</h1>
+      //   <h1>Hello my parent is a div</h1>
+      // </div>
+      <Fragment>
+        <h1>I am an h1 in a fragment</h1>
+      </Fragment>
     );
   }
 }
