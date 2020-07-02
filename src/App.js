a126b819d061e40186c74748392db9c815754e17
Barri <info@bdesignstudio.co.uk>
2020-07-02 18:32:44 +0100

    Add client id and secret for pulling API

--- src/App.js
+++ src/App.js
@@ -6,15 +6,16 @@ import './App.css';
 
 
 class App extends Component {
+ 
   state = {
     users:[],
     loading: false // there is going to be delay white we getting the files so we set loading true while we are grabbing the files as soon as they are ready we turn it to false
   }
       async componentDidMount(){
-        this.setState({loading: true}); // as said above
+          this.setState({loading: true}); // as said above
       
         const res = await axios
-        .get('https://api.github.com/users') // we need this data to become object or state
+        .get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`) // we need this data to become object or state
         // console.log(res.data);
         // after we get the data
         this.setState({users: res.data, loading: false});
