eed5322145c0bb7f4062b7485b67fd10c64ff7c1
Barri <info@bdesignstudio.co.uk>
2020-07-02 12:30:11 +0100

    Change user state to props and fetching data with APP

--- src/App.js
+++ src/App.js
@@ -6,20 +6,30 @@ import './App.css';
 
 
 class App extends Component {
-    componentDidMount(){
-      axios.get('https://api.github.com/users').then(res => console.log(res.data));
-    }
-  render(){
-   
-    return (
-      <div className="App">
-       <Navbar />
-       <div className="container">
-       <Users />
-         
-       </div>
+  state = {
+    users:[],
+    loading: false // there is going to be delay white we getting the files so we set loading true while we are grabbing the files as soon as they are ready we turn it to false
+  }
+      async componentDidMount(){
+        this.setState({loading: true}); // as said above
       
-      </div>
+        const res = await axios
+        .get('https://api.github.com/users') // we need this data to become object or state
+        // console.log(res.data);
+        // after we get the data
+        this.setState({users: res.data, loading: false});
+        }
+      render(){
+      
+        return (
+          <div className="App">
+          <Navbar />
+          <div className="container">
+          <Users loading ={this.state.loading} users ={this.state.users}/>
+            
+          </div>
+          
+          </div>
     
     );
   }
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,32 +1,12 @@
 import React, { Component } from 'react'
 import UserItem from './UserItem';
 export class Users extends Component {
-  state = {
-    users: [
-      {
-        id: '1',
-        login: 'mojombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url:'https://github.com/mojombo'
-      },
-      {
-        id: '22',
-        login: "macournoyer",
-        avatar_url: "https://avatars3.githubusercontent.com/u/21?v=4",
-        html_url: "https://github.com/macournoyer",
-      },{
-        id: '46',
-        login: "bmizerany",
-        avatar_url: "https://avatars2.githubusercontent.com/u/46?v=4",
-        html_url: "https://github.com/bmizerany",
-      }
-    ]
-  }
+  
   render() {
     return (
       <div style={userStyle}>
         {/* expression */}
-        {this.state.users.map(user => (
+        {this.props.users.map(user => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
