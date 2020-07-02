09c3c1847df75a69c943c4d83037171943aa0968
Barri <info@bdesignstudio.co.uk>
2020-07-02 11:45:45 +0100

    Install axios and do the first request

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,22 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-// function UserItem () -- we can trun it to arrow function like below -- user has prop so we need to set it's prop types below. 
-const UserItem=  ({user:{id, login, avatar_url, html_url}}) => {
-    return (
-      <div className='card text-center'>
-        <img src={avatar_url}
+// function UserItem () -- we can turn it to arrow function like below -- user has prop(arguments) so we need to set it's prop types below. 
+const UserItem = ({ user: { id, login, avatar_url, html_url } }) => {
+  return (
+    <div className='card text-center'>
+      <img src={avatar_url}
         alt="" className="round-img"
-        style={{width:'60px'}}
-        />
+        style={{ width: '60px' }}
+      />
 
-    <h3>{id}: {login.toUpperCase()}</h3>
+      <h3>{id}: {login.toUpperCase()}</h3>
 
-    <div>
-      <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
-    </div>
+      <div>
+        <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
       </div>
-    );
-  
+    </div>
+  );
+
 }
 UserItem.propTypes = {
   user: PropTypes.object.isRequired,
